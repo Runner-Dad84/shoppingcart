77012f2797c56f6609c50c68b41b5ca8d6989eca
Runner-Dad84 <vanhoogie@gmail.com>
2025-02-21 14:04:17 -0500

    links to each route

--- src/routes/Cart.jsx
+++ src/routes/Cart.jsx
@@ -1,8 +1,12 @@
+import { Link } from "react-router-dom";
+
 const Cart = () => {
     return (
       <div>
         <h1>Shopping Cart</h1>
         <p>Items go here!</p>
+        <Link to="/shop">Shop Now</Link>
+        <Link to="/">Return</Link>
       </div>
     );
   };
--- src/routes/Shop.jsx
+++ src/routes/Shop.jsx
@@ -1,8 +1,13 @@
+import { Link } from "react-router-dom";
+
+
 const Shop = () => {
     return (
       <div>
         <h1>The Shop</h1>
         <p>Items go here!</p>
+        <Link to="/cart">Cart</Link>
+        <Link to="/">Return</Link>
       </div>
     );
   };
