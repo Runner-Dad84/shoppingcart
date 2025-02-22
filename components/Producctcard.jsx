a858e29ae7b28ccee57ce355d7655850179b87f8
Runner-Dad84 <vanhoogie@gmail.com>
2025-02-21 22:45:50 -0500

    delete old component directory

--- components/Producctcard.jsx
+++
@@ -1,9 +0,0 @@
-export default function Productcard ({ title, url, alt, price }) {
-    return (
-        <>
-         <h1 className="productTitle">{title}</h1>
-         <img src={url} alt={alt} />
-         <h2>{`Price: ${price}`}</h2>
-        </>
-    );
-}
--- src/routes/Shop.jsx
+++ src/routes/Shop.jsx
@@ -1,11 +1,17 @@
 import { Link } from "react-router-dom";
-
+import Productcard from '../components/producctcards.jsx'
 
 const Shop = () => {
     return (
       <div>
         <h1>The Shop</h1>
         <p>Items go here!</p>
+        <Productcard
+        
+        
+        >
+
+        </Productcard>
         <Link to="/cart">Cart</Link>
         <Link to="/">Return</Link>
       </div>
