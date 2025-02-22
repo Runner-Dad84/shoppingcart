98c581f33e2e2d2cd6cc5cac82986e60a1c9ae8b
Runner-Dad84 <vanhoogie@gmail.com>
2025-02-21 22:25:33 -0500

    product card component

--- components/Producctcard.jsx
+++ components/Producctcard.jsx
@@ -1,3 +1,9 @@
-export default Poductcard (){
-    
+export default function Productcard ({ title, url, alt, price }) {
+    return (
+        <>
+         <h1 className="productTitle">{title}</h1>
+         <img src={url} alt={alt} />
+         <h2>{`Price: ${price}`}</h2>
+        </>
+    );
 }
