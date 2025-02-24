feff0f64613c999a7aa908daf189f5a247cb6f69
Runner-Dad84 <vanhoogie@gmail.com>
2025-02-23 21:33:15 -0500

    Add To Cart button

--- src/components/Producctcard.jsx
+++ src/components/Producctcard.jsx
@@ -1,9 +1,17 @@
+
 export default function Productcard ({ title, url, alt, price }) {
+    function add () {
+        console.log('added to cart')
+    }
     return (
         <>
          <h1 className="productTitle">{title}</h1>
          <img src={url} alt={alt} />
          <h2>{`Price: ${price}`}</h2>
+         <button
+         onClick={add}
+         >Add to Cart
+        </button>
         </>
     );
 }
