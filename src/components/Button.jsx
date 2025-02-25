338d7af62604af4a2eafc88859bcf4d2f8289a1f
Runner-Dad84 <vanhoogie@gmail.com>
2025-02-24 21:48:47 -0500

    decrement handler

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -5,6 +5,15 @@ import { useState } from "react";
 export default function SetAmount () {
     const [count, setCount] = useState(0);
 
+    /*decrement handler min of 0 */
+    function decrement () {
+        if (count > 0) {
+            return setCount(count - 1)
+        } else {
+            return setCount(0);
+        }
+    };
+
     return (
         <div>
             <button
@@ -13,7 +22,7 @@ export default function SetAmount () {
             </button>
             <span>{count}</span>
             <button
-            onClick={()=>setCount(count - 1)}
+            onClick={()=>decrement()}
             >-
             </button>
         </div>
