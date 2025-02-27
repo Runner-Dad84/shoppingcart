e5f6340f0fcd2fb95039d054dc58b2c7040e05cf
Runner-Dad84 <vanhoogie@gmail.com>
2025-02-26 21:16:08 -0500

    Add css module

--- src/components/Producctcard.jsx
+++ src/components/Producctcard.jsx
@@ -1,15 +1,46 @@
 import  SetAmount from "./Button";
+import { useState, useEffect } from "react";
+import styles from "../css-modules/productcard.module.css";
 
-
-
-export default function Productcard ({ title, url, alt, price }) {
+export default function Productcard ({ title, alt, price, id }) {
+    const [url, setUrl] = useState(null);
+    const [error, setError] = useState(null);
+    
     function add () {
         console.log('added to cart')
     }
+
+
+    useEffect(() => {
+    const fetchImage = async ()=> {
+        try {
+            const apiKey = '47983806-5d69dad02fddea8f4141376c1';
+            const proxyURL = 'https://corsproxy.io/?';
+            const targetAPI = `http://pixabay.com/api/?key=${apiKey}&id=${id}`;
+            const response = await fetch (proxyURL + targetAPI);
+
+            const data = await response.json();
+            console.log(data);
+            
+
+            if ( data.hits && data.hits.length > 0 ){
+                setUrl(data.hits[0].largeImageURL);
+                console.log(data.hits[0].largeImageURL);
+            } else {
+                throw new Error ('image not found');
+            }
+            } catch (err) {
+                setError(err.message);
+            }}
+            fetchImage();
+        }, []
+    )
+            
+
     return (
         <>
          <h1 className="productTitle">{title}</h1>
-         <img src={url} alt={alt} />
+         <img src={url} alt={alt} className={styles.image}/>
          <h2>{`Price: ${price}`}</h2>
          <SetAmount></SetAmount>
          <button
