aaeea404dd1dd451d172ade34b45b62703bf33d5
Runner-Dad84 <vanhoogie@gmail.com>
2025-02-28 11:16:13 -0500

    Nav component

---
+++ src/components/Nav.jsx
@@ -0,0 +1,19 @@
+import { Link } from "react-router-dom";
+
+export default function Nav () {
+    return (
+    <nav>
+        <ul>
+          <li>
+            <Link to="cart">Cart</Link>
+          </li>
+          <li>
+            <button>
+                <Link to="shop">Shop Now</Link>
+                <span>0</span>
+            </button>
+          </li>
+        </ul>
+      </nav>
+    )
+}
