49b1649fcaf19e0b852cb4f91e58f955ed7d283f
Runner-Dad84 <vanhoogie@gmail.com>
2025-02-28 22:03:52 -0500

    nav css styling with new module

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,20 +1,20 @@
 import { Link } from "react-router-dom";
-import styles from "../css-modules/productcard.module.css";
+import styles from "../css-modules/nav.module.css";
 
 export default function Nav () {
     return (
     <nav className={styles.nav}>
         <ul className={styles.ul}>
-          <li>
+          <li className={styles.li}>
             <Link to="cart">Cart</Link>
           </li>
-          <li>
-            <button>
+          <li className={styles.li}>
+           
                 <Link to="shop">Shop Now</Link>
                 <span>0</span>
-            </button>
+         
           </li>
-          <li>
+          <li className={styles.li}>
              <Link to="/">Return</Link>
           </li>
         </ul>
