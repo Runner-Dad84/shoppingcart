771fbd504e06730ca4696ec588eac7fd71ebb7c4
Runner-Dad84 <vanhoogie@gmail.com>
2025-03-04 22:04:59 -0500

    children instead of props

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -2,12 +2,12 @@ import { createContext, useState} from 'react';
 
 export const CountContext = createContext();
 
-export function CountProvider ( { props }) {
+export function CountProvider ( { children }) {
     const [total, setTotal] = useState(0);
 
     return (
         <CountContext.Provider value={ {total, setTotal }}>
-            {props}
+            {children}
         </CountContext.Provider>
 
     )
