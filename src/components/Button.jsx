6a014b8612bcdf83705e9816f19f0f9c0776cbe1
Runner-Dad84 <vanhoogie@gmail.com>
2025-03-04 21:07:49 -0500

    Input useState

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,24 +1,38 @@
-import { useState } from "react";
+import { useState, useContext } from "react";
 import styles from "../css-modules/productcard.module.css";
+
+
 
 
 
 export default function SetAmount () {
     const [count, setCount] = useState(0);
+    const [input, setInput] = useState(0);
 
     /*decrement handler min of 0 */
     function decrement () {
         if (count > 0) {
-            return setCount(count - 1)
+            setCount(count - 1)
         } else {
-            return setCount(0);
+            setCount(0);
         }
+        
     };
 
+    function increment () {
+        setCount(count + 1);
+       
+    };
+    /*
+    const handleInput = (event) => {
+        const inputVal = setInput(event.target.value);
+        console.log('test')
+    };
+    */
     return (
         <div>
             <button
-            onClick={()=>setCount(count + 1)}
+            onClick={()=>increment()}
             className={styles.addBtn}
             >+
             </button>
@@ -28,9 +42,16 @@ export default function SetAmount () {
             className={styles.subBtn}
             >-
             </button>
+              <label>
+                <input
+                  className={styles.input}
+                  type="number"
+                  min='0'
+                  placeholder={count}
+                  onChange={(e)=>{setInput(e.target.value); setCount(e.target.value)}}
+                />
+              </label>
         </div>
-
-
 
     )
 };
