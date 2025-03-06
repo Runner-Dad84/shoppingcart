fe26cc50aefff1818a0f4485c933d627a3289653
Runner-Dad84 <vanhoogie@gmail.com>
2025-03-05 22:32:33 -0500

    countprovider for Nav to add useContext

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
-import { useState } from 'react'
 import './App.css'
 import Nav from "./components/Nav.jsx";
-import './App.css'
+import { CountProvider } from "./components/Counter.jsx";
 
 export default function App () {
   return (
     <div>
       <h1>Welcome to the Sweetest E-Store On Earth</h1>
-      <Nav></Nav>
+      <CountProvider>
+          <Nav></Nav>
+      </CountProvider>
+      
     </div>
   );
 };
