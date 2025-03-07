5745a4c71e025ebc63b43667db3a0e17dd35d548
Runner-Dad84 <vanhoogie@gmail.com>
2025-03-06 22:09:27 -0500

    Provider set to 0

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -3,10 +3,10 @@ import { createContext, useState} from 'react';
 export const CountContext = createContext();
 
 export function CountProvider ( { children }) {
-    const [total, setTotal] = useState(0);
+    const [count, setCount] = useState(0);
 
     return (
-        <CountContext.Provider value={ {total, setTotal }}>
+        <CountContext.Provider value={ {count, setCount }}>
             {children}
         </CountContext.Provider>
 
