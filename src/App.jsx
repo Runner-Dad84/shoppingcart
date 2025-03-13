548ba6e32a1749ca5909304e7d0555084425fe44
Runner-Dad84 <vanhoogie@gmail.com>
2025-03-12 22:08:01 -0400

    welcome class

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import styles from './css-modules/welcome.module.css'
 
 export default function App () {
   return (
-    <div>
+    <div className={styles.welcome}>
       <h1>Welcome to the Sweetest E-Store On Earth</h1>
       <div className={styles.welcomeImg}></div>
           <Nav></Nav>
