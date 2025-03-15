d2d0a18380fa1990739482b5b467f985b9976d96
Runner-Dad84 <vanhoogie@gmail.com>
2025-03-14 22:40:11 -0400

    render test using utility function

--- tests/App.test.jsx
+++ tests/App.test.jsx
@@ -1,18 +1,27 @@
 import { describe, it, expect } from 'vitest';
-import { MemoryRouter, Route, Routes } from 'react-router-dom';
 import Shop from '/src/routes/Shop.jsx';
-import { CountProvider } from '/src/components/Counter.jsx'
 import { render, screen } from '@testing-library/react';
+//import UserEvent from '@testing-library/user-event';
+import { renderWithProvider } from './test-utils.jsx'
+
+//Test the rendering of the shop page
+test('check if running', ()=> {
+  expect(true).toBe(true);
+  
+});
+
 
 test('renders shop page', async () => {
-  render(
-   <CountProvider>
-    <MemoryRouter initialEntries={['/Shop']}>
-      <Routes>
-        <Route path="/Shop" element={<Shop />} />
-      </Routes>
-    </MemoryRouter>
-    </CountProvider>
-  );
+  
+  renderWithProvider(<Shop />)
+  screen.debug();
+ 
 }
 );
+
+describe('Testing App Component', () => {
+  test('Increment by one on button click', async () => {
+
+})
+
+})
