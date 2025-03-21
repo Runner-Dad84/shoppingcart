0aeb36d7fb5d64df9c26814d480e0add390121bd
Runner-Dad84 <vanhoogie@gmail.com>
2025-03-20 22:09:15 -0400

    test increment buttons and span values

--- tests/App.test.jsx
+++ tests/App.test.jsx
@@ -1,7 +1,7 @@
 import { describe, it, expect } from 'vitest';
 import Shop from '/src/routes/Shop.jsx';
 import { render, screen } from '@testing-library/react';
-//import UserEvent from '@testing-library/user-event';
+import userEvent from '@testing-library/user-event';
 import { renderWithProvider } from './test-utils.jsx'
 
 //Test the rendering of the shop page
@@ -11,17 +11,26 @@ test('check if running', ()=> {
 });
 
 
-test('renders shop page', async () => {
-  
+describe('Testing Shop Page', () => {
   renderWithProvider(<Shop />)
-  screen.debug();
- 
-}
-);
-
-describe('Testing App Component', () => {
-  test('Increment by one on button click', async () => {
-
+  
+test('renders shop page', async () => {
+  renderWithProvider(<Shop />)
+  
+});
+test('Increment by one on button click', async () => {
+  renderWithProvider(<Shop />)
+  
+  //(1) Make array of buttons using getAllByRole
+  const buttons = await screen.findAllByTestId('plus');
+  //(2) Click each button in order
+  for (const button of buttons) {
+    await userEvent.click(button);
+  };
+  //(3) Make array of <span> values useing getAllByRole
+  const spanVals = await screen.findAllByTestId('itemValue');
+  //(4) Test each span val increased from 0 to 1
+  const values = spanVals.map(item => item.innerHTML);
+  expect(values).not.toContain('0');
 })
-
 })
